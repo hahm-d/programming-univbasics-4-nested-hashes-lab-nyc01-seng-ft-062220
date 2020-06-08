0a0b235a0f365eb842f1a8e1972162e29a50830c
Daniel Hahm <dhahm01@gmail.com>
2020-06-08 21:28:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,6 @@ def adding_to_dennis
       }
     }
 
-  programmer_hash[:dennis_ritchie][:known_for] = ['Unix', 'Assembly']
+  programmer_hash[:dennis_ritchie][:languages] = ['C', 'Assembly']
   return programmer_hash
 end
